8fb1e7ad4d8a854aab091898d4d93994ea37c796
Jesse Plamondon-Willard <github@jplamondonw.com>
2015-10-22 14:52:53 -0400

    add empty Visual Studio console app

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pathoschild.SlackArchiveSearch")]
+[assembly: AssemblyDescription("A Windows console application which parses and searches an exported Slack archive.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pathoschild.SlackArchiveSearch")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7b01d673-c251-49cd-89c3-b677d1e09b58")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
